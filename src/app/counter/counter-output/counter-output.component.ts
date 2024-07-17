7a62b3f95b7b06bbf20c589501c83b82a704fbb9
satya-p14 <vikkisatya@gmail.com>
2024-07-17 19:04:20 +0530

    added async pipe
    removed subscriptions .
    getting data using store

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,30 +1,24 @@
 import { Component, Input, OnDestroy, OnInit, } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { counterState } from '../state/counter.state';
-import { Subscription } from 'rxjs';
-
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-counter-output',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './counter-output.component.html',
   styleUrl: './counter-output.component.scss'
 })
-export class CounterOutputComponent implements OnInit, OnDestroy {
-  counter: number = 0;  
-  counterSubs!:Subscription
+export class CounterOutputComponent implements OnInit {
+  counter: number = 0;
+  counter$:Observable<counterState> = new Observable();
   constructor(private store: Store<{ counter: counterState }>) {
   } 
 
   public ngOnInit(): void {
-    this.counterSubs = this.store.select('counter').subscribe(data =>{
-      this.counter = data.counter;
-    });
-  }
-
-  public ngOnDestroy(): void {
-    this.counterSubs.unsubscribe();
+    this.counter$ = this.store.select("counter");
   }
 
 }
